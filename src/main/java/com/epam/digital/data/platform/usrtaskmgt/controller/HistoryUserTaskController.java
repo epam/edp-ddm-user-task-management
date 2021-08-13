f4f499fee931698684a94ab44b01e6e0226e98d0
Ruslan Lesyk <ruslan_lesyk@epam.com>
2021-08-13 11:37:18 +0000

    [EDP-DDM-8166] added pagination/sorting for history tasks

--- src/main/java/com/epam/digital/data/platform/usrtaskmgt/controller/HistoryUserTaskController.java
+++ src/main/java/com/epam/digital/data/platform/usrtaskmgt/controller/HistoryUserTaskController.java
@@ -6,8 +6,10 @@ import com.epam.digital.data.platform.usrtaskmgt.service.HistoryUserTaskService;
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.camunda.bpm.engine.rest.dto.CountResultDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +24,16 @@ public class HistoryUserTaskController {
   @Operation(
       summary = "Retrieve all completed tasks",
       description = "Returns completed task list")
-  public List<HistoryUserTaskDto> getHistoryTasks() {
-    return historyUserTaskService.getHistoryTasks();
+  public List<HistoryUserTaskDto> getHistoryTasks(@RequestParam(required = false) Integer firstResult,
+      @RequestParam(required = false) Integer maxResults,
+      @RequestParam(required = false) String sortBy,
+      @RequestParam(required = false) String sortOrder) {
+    return historyUserTaskService.getHistoryTasks(firstResult, maxResults, sortBy, sortOrder);
+  }
+
+  @GetMapping("/count")
+  @Operation(summary = "Retrieve count of all finished tasks", description = "Returns finished tasks count")
+  public CountResultDto countHistoryTasks() {
+    return historyUserTaskService.countHistoryTasks();
   }
 }
--- src/main/java/com/epam/digital/data/platform/usrtaskmgt/service/HistoryUserTaskServiceImpl.java
+++ src/main/java/com/epam/digital/data/platform/usrtaskmgt/service/HistoryUserTaskServiceImpl.java
@@ -2,6 +2,7 @@ package com.epam.digital.data.platform.usrtaskmgt.service;
 
 import static java.util.Objects.nonNull;
 
+import com.epam.digital.data.platform.bpms.api.dto.HistoryTaskCountQueryDto;
 import com.epam.digital.data.platform.bpms.api.dto.HistoryTaskQueryDto;
 import com.epam.digital.data.platform.bpms.api.dto.ProcessDefinitionQueryDto;
 import com.epam.digital.data.platform.bpms.client.HistoryTaskRestClient;
@@ -10,11 +11,11 @@ import com.epam.digital.data.platform.usrtaskmgt.dto.HistoryUserTaskDto;
 import com.epam.digital.data.platform.usrtaskmgt.mapper.UserTaskDtoMapper;
 import com.epam.digital.data.platform.usrtaskmgt.util.AuthUtil;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.engine.impl.persistence.entity.HistoricTaskInstanceEntity;
+import org.camunda.bpm.engine.rest.dto.CountResultDto;
 import org.camunda.bpm.engine.rest.dto.repository.ProcessDefinitionDto;
 import org.springframework.stereotype.Service;
 
@@ -27,14 +28,27 @@ public class HistoryUserTaskServiceImpl implements HistoryUserTaskService {
   private final UserTaskDtoMapper userTaskDtoMapper;
 
   @Override
-  public List<HistoryUserTaskDto> getHistoryTasks() {
+  public List<HistoryUserTaskDto> getHistoryTasks(Integer firstResult, Integer maxResults, String sortBy,
+      String sortOrder) {
     List<HistoricTaskInstanceEntity> historyTasksByParams = historyTaskRestClient
         .getHistoryTasksByParams(
             HistoryTaskQueryDto.builder()
+                .firstResult(firstResult)
+                .maxResults(maxResults)
+                .sortBy(sortBy)
+                .sortOrder(sortOrder)
                 .taskAssignee(AuthUtil.getCurrentUsername())
                 .finished(true)
-            .build());
+                .build());
     return postProcess(userTaskDtoMapper.toHistoryUserTasks(historyTasksByParams));
+  }
+
+  @Override
+  public CountResultDto countHistoryTasks() {
+    return historyTaskRestClient.getHistoryTaskCountByParams(HistoryTaskCountQueryDto.builder()
+        .taskAssignee(AuthUtil.getCurrentUsername())
+        .finished(true)
+        .build());
   }
 
   private List<HistoryUserTaskDto> postProcess(List<HistoryUserTaskDto> historyUserTaskDtos) {
@@ -43,18 +57,18 @@ public class HistoryUserTaskServiceImpl implements HistoryUserTaskService {
   }
 
   private void fillProcessDefinitionName(List<HistoryUserTaskDto> historyUserTaskDtos) {
-    List<String> processDefinitionIds = historyUserTaskDtos.stream()
+    var processDefinitionIds = historyUserTaskDtos.stream()
         .map(HistoryUserTaskDto::getProcessDefinitionId)
         .distinct().collect(Collectors.toList());
-    ProcessDefinitionQueryDto processDefinitionQueryDto = ProcessDefinitionQueryDto.builder()
+    var processDefinitionQueryDto = ProcessDefinitionQueryDto.builder()
         .processDefinitionIdIn(processDefinitionIds)
         .build();
-    List<ProcessDefinitionDto> processDefinitions = processDefinitionRestClient
+    var processDefinitions = processDefinitionRestClient
         .getProcessDefinitionsByParams(processDefinitionQueryDto);
-    Map<String, ProcessDefinitionDto> processDefinitionIdAndDtoMap = processDefinitions.stream()
+    var processDefinitionIdAndDtoMap = processDefinitions.stream()
         .collect(Collectors.toMap(ProcessDefinitionDto::getId, Function.identity()));
     historyUserTaskDtos.forEach(task -> {
-      ProcessDefinitionDto processDefinitionDto = processDefinitionIdAndDtoMap
+      var processDefinitionDto = processDefinitionIdAndDtoMap
           .get(task.getProcessDefinitionId());
       if (nonNull(processDefinitionDto)) {
         task.setProcessDefinitionName(processDefinitionDto.getName());
